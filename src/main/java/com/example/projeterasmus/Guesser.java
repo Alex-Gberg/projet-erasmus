80ab230a6da3e1c544c9d6b70cbc2fdcfc5eb7b9
Alex-Gberg <alexandergoldberg97@gmail.com>
2022-02-14 16:45:28 +0100

    Made guess selector

--- src/main/java/com/example/projeterasmus/Guesser.java
+++ src/main/java/com/example/projeterasmus/Guesser.java
@@ -7,22 +7,22 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Guesser {
+    int numRows;
+    int numColumns;
     JsonObject root;
     HBox guesser;
+    HashMap<String, Set<String>> propMap;
     ObservableList<String> properties;
     ComboBox<String> propertySelector;
+    ComboBox<String> valueSelector;
     Button guessButton;
 
     public Guesser(String jsonName) {
@@ -33,6 +33,8 @@ public class Guesser {
             Gson gson = new Gson();
             JsonElement json = gson.fromJson(bufferedReader, JsonElement.class);
             root = json.getAsJsonObject();
+            this.numRows = root.get("ligne").getAsInt();
+            this.numColumns = root.get("column").getAsInt();
         }
         catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -41,6 +43,8 @@ public class Guesser {
         findProperties();
         guesser = new HBox();
         propertySelector = new ComboBox<>(properties);
+        valueSelector = new ComboBox<>();
+        propertySelector.setOnAction(e -> valueSelector.setItems(FXCollections.observableArrayList(propMap.get(propertySelector.getSelectionModel().getSelectedItem()))));
         guessButton = new Button("Guess!");
         constructGeusser();
     }
@@ -48,12 +52,23 @@ public class Guesser {
     private void findProperties() {
         Set<String> propSet = root.getAsJsonObject("personnages").getAsJsonObject("0").keySet();
         propSet.remove("fichier");
-
+        propMap = new HashMap<>();
+        for (String s : propSet) {
+            propMap.put(s, new HashSet<>());
+        }
         properties = FXCollections.observableArrayList(propSet);
+
+        JsonObject pers = root.getAsJsonObject("personnages");
+        for (int i = 0; i < numRows*numColumns; i++) {
+            for (String s : propSet) {
+                propMap.get(s).add(pers.getAsJsonObject(String.valueOf(i)).get(s).getAsString());
+            }
+        }
+        System.out.println(propMap);
     }
 
     private void constructGeusser() {
-        guesser.getChildren().addAll(propertySelector, guessButton);
+        guesser.getChildren().addAll(propertySelector, valueSelector, guessButton);
     }
 
     public HBox getGuesser() {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.projeterasmus {
     requires javafx.controls;
     requires javafx.fxml;
     requires com.google.gson;
+    requires java.desktop;
 
 
     opens com.example.projeterasmus to javafx.fxml;
